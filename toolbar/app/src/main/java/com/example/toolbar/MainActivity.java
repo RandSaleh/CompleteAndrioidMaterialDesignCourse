bd72087e3eabcc31a32f7936eb1227c78e6628a5
RandSaleh <rand_nn@hotmail.com>
2019-02-11 21:14:05 +0200

    add popupMenue

--- toolbar/app/src/main/java/com/example/toolbar/MainActivity.java
+++ toolbar/app/src/main/java/com/example/toolbar/MainActivity.java
@@ -1,16 +1,40 @@
 package com.example.toolbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-Toolbar toolbar;
+    Toolbar toolbar;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()==R.id.itm1)
+            Toast.makeText(this,"item1",Toast.LENGTH_LONG).show();
+        else
+            if(item.getItemId()==R.id.itm2)
+                Toast.makeText(this,"item2",Toast.LENGTH_LONG).show();
+
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar=findViewById(R.id.toolbar);
-        
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
